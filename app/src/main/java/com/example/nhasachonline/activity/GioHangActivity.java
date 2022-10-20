41eab5f85c13f2a2c960d9400e86dfc409a6ac01
HieuLHBT <hieulhbt@gmail.com>
2022-10-20 21:58:06 +0700

    Man hinh gio hang (xong)

--- app/src/main/java/com/example/nhasachonline/activity/GioHangActivity.java
+++ app/src/main/java/com/example/nhasachonline/activity/GioHangActivity.java
@@ -1,6 +1,115 @@
 package com.example.nhasachonline.activity;
 
-import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class GioHangActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nhasachonline.R;
+import com.example.nhasachonline.adapters.GioHangRecyclerViewAdapter;
+import com.example.nhasachonline.doituong.GioHang;
+
+import java.util.ArrayList;
+
+public class GioHangActivity extends AppCompatActivity {
+    private int selectedRow = -1;
+    private Drawable backBackground;
+    private CardView previousItem;
+
+    private ArrayList<GioHang> gioHangs = new ArrayList<>();
+    private GioHangRecyclerViewAdapter adapter;
+
+    private TextView layoutGH_tvTongTienThanhToan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.giohang_layout);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.layoutGH_rvDanhSach);
+        layoutGH_tvTongTienThanhToan = findViewById(R.id.layoutGH_tvTongTienThanhToan);
+
+        gioHangs.add(new GioHang("Sach 1", 30000, 10, 3));
+        gioHangs.add(new GioHang("Sach 2", 60000, 5, 1));
+        gioHangs.add(new GioHang("Sach 3", 20000, 30, 2));
+
+        adapter = new GioHangRecyclerViewAdapter(this, R.layout.giohang_item, gioHangs);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter.setOnItemClickListener(new GioHangRecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClickListener(int position, View view) {
+                if (selectedRow == -1) {
+                    selectedRow = position;
+                    // Change the item background color
+                    CardView cvItem = view.findViewById(R.id.itemGH);
+                    backBackground = cvItem.getBackground();
+                    cvItem.setBackgroundColor(getResources().getColor(R.color.teal_700, getTheme()));
+                    previousItem = cvItem;
+                } else {
+                    if (selectedRow == position) {//Tap on the same item
+                        selectedRow = -1;
+                        CardView cvItem = view.findViewById(R.id.itemGH);
+                        cvItem.setBackground(backBackground);
+                    } else {// Tap on other item
+                        previousItem.setBackground(backBackground);
+                        selectedRow = position;
+                        CardView cvItem = view.findViewById(R.id.itemGH);
+                        cvItem.setBackgroundColor(getResources().getColor(R.color.teal_700, getTheme()));
+                        previousItem = cvItem;
+                    }
+                }
+
+                ImageButton itemGH_btnTru = view.findViewById(R.id.itemGH_btnTru);
+                ImageButton itemGH_btnCong = view.findViewById(R.id.itemGH_btnCong);
+                TextView itemGH_tvTongTien = view.findViewById(R.id.itemGH_tvTongTien);
+                itemGH_btnTru.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (gioHangs.get(position).getSoLuongSanPham() == 1) {
+                            gioHangs.remove(position);
+                            adapter.notifyDataSetChanged();
+                            TongTienThanhToan();
+                        } else if (gioHangs.get(position).getSoLuongSanPham() > 1) {
+                            gioHangs.get(position).setSoLuongSanPham(gioHangs.get(position).getSoLuongSanPham() - 1);
+                            gioHangs.get(position).setTongTien((gioHangs.get(position).getGiaSanPham() - (gioHangs.get(position).getGiaSanPham() * gioHangs.get(position).getKhuyenMai() / 100)) * gioHangs.get(position).getSoLuongSanPham());
+                            adapter.notifyDataSetChanged();
+                            TongTienThanhToan();
+                        }
+                    }
+                });
+                itemGH_btnCong.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gioHangs.get(position).setSoLuongSanPham(gioHangs.get(position).getSoLuongSanPham() + 1);
+                        gioHangs.get(position).setTongTien((gioHangs.get(position).getGiaSanPham() - (gioHangs.get(position).getGiaSanPham() * gioHangs.get(position).getKhuyenMai() / 100)) * gioHangs.get(position).getSoLuongSanPham());
+                        adapter.notifyDataSetChanged();
+                        TongTienThanhToan();
+                    }
+                });
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+
+        TongTienThanhToan();
+
+    }
+
+    private void TongTienThanhToan() {
+        Integer sum = 0;
+        for (int i = 0; i < gioHangs.size(); i++) {
+            sum += gioHangs.get(i).getTongTien();
+        }
+        layoutGH_tvTongTienThanhToan.setText(sum + "");
+    }
 }
