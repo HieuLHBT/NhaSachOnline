cd0a73b47b9b9b877100595b371d3efb53d609ad
TruongDinhTho <43953312+TruongDinhTho@users.noreply.github.com>
2022-10-24 07:13:59 +0700

    layout man hinh chinh kh, chi tiet kh

--- app/src/main/java/com/example/nhasachonline/adapters/ManHinhChinhKhachHangAdapter.java
+++ app/src/main/java/com/example/nhasachonline/adapters/ManHinhChinhKhachHangAdapter.java
@@ -3,11 +3,14 @@ package com.example.nhasachonline.adapters;
 import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.nhasachonline.R;
 import com.example.nhasachonline.doituong.Sach;
@@ -15,40 +18,105 @@ import com.example.nhasachonline.doituong.Sach;
 import java.util.ArrayList;
 
 
-public class ManHinhChinhKhachHangAdapter extends ArrayAdapter<Sach> {
+public class ManHinhChinhKhachHangAdapter extends RecyclerView.Adapter<ManHinhChinhKhachHangAdapter.MyViewHolder> {
     private Activity context;
     private int resource;
-    final ArrayList<Sach> listSach;
+    private ArrayList<Sach> books;
+    private GioHangRecyclerViewAdapter.OnItemClickListener onItemClickListener;
 
-    public ManHinhChinhKhachHangAdapter(@NonNull Activity context, int resource, @NonNull ArrayList<Sach> listSach) {
-        super(context, resource, listSach);
+    public ManHinhChinhKhachHangAdapter(Activity context, int resource, ArrayList<Sach> books) {
         this.context = context;
         this.resource = resource;
-        this.listSach = listSach;
+        this.books = books;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view = null;
-        MyViewHolder viewHolder;
-        if(convertView == null){
-            view =context.getLayoutInflater().inflate(resource, parent,false);
-            viewHolder = new MyViewHolder();
-            //get view item from view object
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        CardView viewItem = (CardView) context.getLayoutInflater().inflate(viewType, parent, false);
+        return new MyViewHolder(viewItem);
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        final int pos = position;
+        Sach book = books.get(pos);
+        holder.itemMHCKH_tvTenSanPham.setText(book.getTenSach());
+        holder.itemMHCKH_tvGia.setText(book.getGia() + "");
+        holder.itemMHCKH_tvSoLuong.setText(book.getSoLuong() + "");
+        holder.itemMHCKH_tvDanhGia.setText(book.getsoLuongdanhGia() + "");
+
+
+        // Event processing
+        holder.onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClickListener(pos, holder.itemView);
+                }
+            }
+        };
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return resource;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView itemMHCKH_tvTenSanPham;
+        TextView itemMHCKH_tvGia;
+        TextView itemMHCKH_tvDanhGia;
+        TextView itemMHCKH_tvSoLuong;
+        ImageView itemMHCKH_imgHinhSanPham;
+        ImageView itemMHCKH_img1Sao;
+        ImageView itemMHCKH_img2Sao;
+        ImageView itemMHCKH_img3Sao;
+        ImageView itemMHCKH_img4Sao;
+        ImageView itemMHCKH_img5Sao;
+        ImageButton itemMHCKH_btnThemVaoGioHang;
+        View.OnClickListener onClickListener;
+        LinearLayout itemMHCKH_llCardView;
+        CardView itemMHCKH;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            itemMHCKH_tvTenSanPham = itemView.findViewById(R.id.itemMHCKH_tvTenSanPham);
+            itemMHCKH_tvGia = itemView.findViewById(R.id.itemMHCKH_tvGiaTien);
+            itemMHCKH_tvDanhGia = itemView.findViewById(R.id.itemMHCKH_tvSLDanhGia);
+            itemMHCKH_tvSoLuong = itemView.findViewById(R.id.itemMHCKH_tvSoLuong);
+            itemMHCKH_imgHinhSanPham = itemView.findViewById(R.id.itemMHCKH_imgAnhSanPham);
+            itemMHCKH_img1Sao = itemView.findViewById(R.id.itemMHCKH_img1Sao);
+            itemMHCKH_img2Sao = itemView.findViewById(R.id.itemMHCKH_img2Sao);
+            itemMHCKH_img3Sao = itemView.findViewById(R.id.itemMHCKH_img3Sao);
+            itemMHCKH_img4Sao = itemView.findViewById(R.id.itemMHCKH_img4Sao);
+            itemMHCKH_img5Sao = itemView.findViewById(R.id.itemMHCKH_img5Sao);
+            itemMHCKH_btnThemVaoGioHang = itemView.findViewById(R.id.itemMHCKH_btnThemGioHang);
+            itemMHCKH_llCardView = itemView.findViewById(R.id.itemMHCKH_llCardView);
+            itemMHCKH = itemView.findViewById(R.id.itemManHinhChinhKH);
+
+            // Set event processing
+            itemMHCKH_btnThemVaoGioHang.setOnClickListener(this);
         }
 
-        return view;
-
+        public void onClick(View view) {
+            if (onClickListener != null) {
+                onClickListener.onClick(view);
+            }
+        }
     }
-    public static class MyViewHolder{
-        TextView tvTenSP;
-        TextView tvTacGia;
-        TextView tvGia;
-        TextView tvKM;
-        TextView tvGKM;
-        TextView tvSoLuong;
+    // Interface for event processing
+    public interface OnItemClickListener {
+        void onItemClickListener(int position, View view);
+    }
 
+    public void setOnItemClickListener(GioHangRecyclerViewAdapter.OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 }
